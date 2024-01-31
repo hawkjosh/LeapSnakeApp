8b05da85fe1e19915a46d51dd781c2fc9519f90f
Shruthi Parthasarathy <t-shruthip@microsoft.com>
2024-01-31 12:47:43 -0800

    Adding tile, speed and direction enums for snake game

---
+++ SnakeApp/Models/Speed.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeApp.Models
+{
+    public enum Speed
+    {
+        Slow = 0,
+        Medium,
+        Fast
+    }
+}
---
+++ SnakeApp/Models/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeApp.Models
+{
+    public enum Tile
+    {
+        Empty,
+        Snake,
+        Food
+    }
+}
